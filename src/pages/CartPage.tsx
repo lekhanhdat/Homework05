9d8ce1a5b3eebd01c6a943fce67b90913ff2e80f
augustine.le <augustine.le@avepoint.com>
2026-08-11 11:35:07 +0700

    feat: render cart contents

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,5 +1,35 @@
+import { useSelector } from 'react-redux'
+import type { RootState } from '../app/store'
+
+const currencyFormatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+})
+
 function CartPage() {
-  return <h1>Shopping Cart</h1>
+  const cartItems = useSelector((state: RootState) => state.cart.items)
+
+  return (
+    <section>
+      <h1>Shopping Cart</h1>
+      <ul className="cart-list">
+        {cartItems.map((item) => (
+          <li className="cart-item" key={item.id}>
+            <div>
+              <h2>{item.name}</h2>
+              <p className="cart-item-price">
+                {currencyFormatter.format(item.price)}
+              </p>
+            </div>
+            <p className="cart-item-quantity">Quantity: {item.quantity}</p>
+            <p className="cart-item-subtotal">
+              Subtotal: {currencyFormatter.format(item.price * item.quantity)}
+            </p>
+          </li>
+        ))}
+      </ul>
+    </section>
+  )
 }
 
 export default CartPage
