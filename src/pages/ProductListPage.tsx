60325a554eda04e74adb40fe543b847c8a2a8015
augustine.le <augustine.le@avepoint.com>
2026-08-11 10:42:08 +0700

    feat: render product list page

--- src/pages/ProductListPage.tsx
+++ src/pages/ProductListPage.tsx
@@ -1,5 +1,27 @@
+import { products } from '../data/products'
+
+const currencyFormatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+})
+
 function ProductListPage() {
-  return <h1>Products</h1>
+  return (
+    <section>
+      <h1>Products</h1>
+      <ul className="product-list">
+        {products.map((product) => (
+          <li className="product-item" key={product.id}>
+            <h2>{product.name}</h2>
+            <p className="product-price">
+              {currencyFormatter.format(product.price)}
+            </p>
+            <p>{product.description}</p>
+          </li>
+        ))}
+      </ul>
+    </section>
+  )
 }
 
 export default ProductListPage
