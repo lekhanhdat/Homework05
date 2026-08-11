9f502c9db22aa6cfe8da0122e22a00fb485e1d47
augustine.le <augustine.le@avepoint.com>
2026-08-11 15:13:34 +0700

    refactor: convert global styles to scss

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
-import './index.css'
+import './index.scss'
 import { store } from './app/store'
 import App from './App.tsx'
 
